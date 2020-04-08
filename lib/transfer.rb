0d998d5fc3c99b97092f8638867411d873147421
Ray Alva <rayalva407@gmail.com>
2020-04-08 20:45:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end  
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction
+   BankAccount.balance += amount
+  end
+  
+  def reverse_transfer
+    
+  end
 end
