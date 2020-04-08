60ea61a617e23736ae3ec40f5583fa1d6eb7d1cf
Ray Alva <rayalva407@gmail.com>
2020-04-08 21:28:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
   
   def execute_transaction
-   if sender.balance > @amount && @status == "pending"
+   if sender.balance > @amount && @status == "pending" && valid?
      sender.balance -= @amount
      receiver.balance += @amount
      @status = "complete"
